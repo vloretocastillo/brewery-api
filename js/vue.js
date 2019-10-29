012c910f7ff33439df167342650bf84e25180f8c
valentinaloreto <vloretocastillo@gmail.com>
2019-10-29 14:51:38 +0100

    added vue instance

--- js/vue.js
+++ js/vue.js
@@ -5,34 +5,34 @@ let app = new Vue({
         data : [],
         current : {},
         page : 1,
-        per_page : 80
+        per_page : 80,
+        displayDetailedView : false
     },
 
-    computed : {
-        
-        
-    },
+    computed : {},
 
     methods : {
-        updateValues : function (id) {
+        detailedView : function (id) {
             this.current = this.data.find(el => el.id == id)
-            // console.log(this.current.name)
+            this.displayDetailedView = true
         },
 
         getData : async function () {
             this.data = await fetch(`https://api.punkapi.com/v2/beers?page=${this.page}&per_page=${this.per_page}`, {
                 method: 'GET',
             })
-            .then(res => res.json())
+            .then(res => {
+                this.page +=1 
+                return res.json()
+            })
             .catch(err => console.error(err)) 
         },
     },
 
     created : function () {  
+
         this.getData()
-            .then(()=> {
-                this.page +=1 
-            })
+            
     },
 
 })
